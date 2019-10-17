257f80d391953cd719cba10762db4bd61e2c2d18
youngsoohan <amazingmobdev@gmail.com>
2019-10-18 01:56:09 +0900

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RNBootSplash from 'react-native-bootsplash';
 import styled from 'styled-components/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { Text, ScreenWrap } from '../components';
+import { Text, ScreenWrap } from './components';
 
 const Wrap = styled.View`
   flex: 1;
