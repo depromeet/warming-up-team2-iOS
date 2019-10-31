402e105cb2914d2bb3fd7367cab3586dfd89e86e
youngsoohan <amazingmobdev@gmail.com>
2019-10-31 13:40:32 +0900

    implment graph

--- src/screens/Mypage.tsx
+++ src/screens/Mypage.tsx
@@ -1,16 +1,55 @@
 import React from 'react';
-import styled from 'styled-components/native';
 
-import { Text } from 'components';
+import styled from 'styled-components/native';
+import { BarChart, XAxis } from 'react-native-svg-charts';
+import { Defs, LinearGradient, Stop } from 'react-native-svg';
 
 const Wrap = styled.View`
   flex: 1;
 `;
 
 const Mypage: React.FC = () => {
+  const data = [0, 0, 0, 0, 0, 0];
+  const newData = [50, 54, 60, 95, 60, 30];
+  const [datas, setDats] = React.useState(data);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setDats(newData);
+    }, 4400);
+  }, []);
+
+  const Gradient = () => (
+    <Defs key="gradient">
+      <LinearGradient id="gradient" x1="0%" x2="0%" y2="100%">
+        <Stop offset="0%" stopColor="#00aef2" />
+        <Stop offset="100%" stopColor="#ffffff" />
+      </LinearGradient>
+    </Defs>
+  );
+
   return (
     <Wrap>
-      <Text>Mypage</Text>
+      <BarChart
+        animate
+        animationDuration={1000}
+        spacingInner={0.9}
+        style={{ height: 200, marginHorizontal: 20 }}
+        data={datas}
+        svg={{
+          fill: 'url(#gradient)',
+        }}
+        contentInset={{ top: 20, bottom: 20 }}
+      >
+        <Gradient />
+      </BarChart>
+      <XAxis
+        style={{ marginHorizontal: 10, marginTop: 10 }}
+        data={data}
+        formatLabel={(value, index) => index}
+        contentInset={{ left: 15, right: 15 }}
+        svg={{ fontSize: 12, fill: '#454545' }}
+      />
     </Wrap>
   );
 };
