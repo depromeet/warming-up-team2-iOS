607c595e202b717937bdd07bfbf02a4421a4b302
youngsoohan <amazingmobdev@gmail.com>
2019-11-04 16:32:30 +0900

    implement login view

--- src/libs/icons.ts
+++ src/libs/icons.ts
@@ -10,4 +10,6 @@ export const IC_EDIT = require('icons/ic_edit.png');
 export const IMG_CHA_3 = require('imgs/img_character_3.png');
 export const IMG_CHA_4 = require('imgs/img_character_4.png');
 export const IMG_BG_CHA = require('imgs/bg_cha.png');
+export const IMG_LOGIN = require('imgs/img_login.png');
+export const IMG_BT_LOGIN = require('imgs/bt_lgin.png');
 export const SPLASH_IMG = require('imgs/splash.gif');
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -5,7 +5,11 @@ import styled from 'styled-components/native';
 
 import * as NavigationService from 'libs/NavigationService';
 import colors from 'libs/colors';
-import { ScreenWrap, Text, Touchable } from 'components';
+import { ScreenWrap, Touchable } from 'components';
+import { IMG_LOGIN, IMG_BT_LOGIN, IMG_BG_CHA } from 'libs/icons';
+import { DEVICE_WIDTH } from 'libs/styleUtils';
+
+const IMAGE_HEIGHT = DEVICE_WIDTH * 0.5;
 
 const Wrap = styled.View`
   flex: 1;
@@ -14,7 +18,28 @@ const Wrap = styled.View`
   align-items: center;
 `;
 
+const LoginCenter = styled.Image.attrs({ source: IMG_LOGIN })``;
+
+const LoginBtnImage = styled.Image.attrs({ source: IMG_BT_LOGIN })``;
+
 const LoginButton = styled(Touchable)``;
+
+const BGImage = styled.Image.attrs({ source: IMG_BG_CHA })`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: ${IMAGE_HEIGHT}px;
+  opacity: 0.2;
+`;
+
+const CenterView = styled.View`
+  height: 220px;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
 
 const Login: NavigationStackScreenComponent = () => {
   const onPressLogin = () => {
@@ -29,12 +54,16 @@ const Login: NavigationStackScreenComponent = () => {
   };
 
   return (
-    <ScreenWrap>
+    <ScreenWrap forceInset={{ bottom: 'never' }}>
       <Wrap>
-        <LoginButton onPress={onPressLogin}>
-          <Text>로그인하기</Text>
-        </LoginButton>
+        <CenterView>
+          <LoginCenter />
+          <LoginButton onPress={onPressLogin}>
+            <LoginBtnImage />
+          </LoginButton>
+        </CenterView>
       </Wrap>
+      <BGImage />
     </ScreenWrap>
   );
 };
